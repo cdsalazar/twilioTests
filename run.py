2444127b283f07f701722ef4557cc3b9c3abcbf9
Alexander Tsankov <antsankov@gmail.com>
2014-11-07 16:21:14 -0700

    Got a basic parser working

--- run.py
+++ run.py
@@ -10,6 +10,7 @@ client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
 app = Flask(__name__)
 
+#this is the same parser, makes ure that we include these helper functions before the app route is defined!
 def parser(input):
     mod_input = input.split()
     if (mod_input[0] == "hello" and mod_input[1] == "world"):
@@ -18,23 +19,24 @@ def parser(input):
     else:
         return "we not cool"
 
+#this actually crafts the message for the person. Currently it grabs all messages and only selects the first inbound one, we should find a way to reduce this
 def returner():
     messages = client.messages.list() 
     for m in messages: 
         if (m.direction == 'inbound'):
             return parser(m.body)
-            #return m.body
-
  
+#this is the main route with the two possible verbs 
 @app.route("/", methods=['GET', 'POST'])
 
-
+#this is the responder function 
 def hello_monkey():
     resp = twilio.twiml.Response()
     resp.message("R: " + returner())
     return str(resp)
 
 
+#this gets the server running.
 if __name__ == "__main__":
     app.run(debug=True)
 
